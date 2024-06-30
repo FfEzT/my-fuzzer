81c8f9c91dc504770c90f078690987387e868538
ffezt <vl.al.kim@mail.ru>
2024-06-30 13:49:21 +0300

    fix flag description

--- internal/config/flag/flag.go
+++ internal/config/flag/flag.go
@@ -49,18 +49,16 @@ type Range struct {
 // }
 
 func GetConfig() (result *Config, err error) {
-  result = &Config{}
-
-  target      := flag.String("u", "foo", "host target")
+  target      := flag.String("u", "", "host target")
   worlistPath := flag.String("w", "", "Path to wordlist")
   payload     := flag.String("d", "", "payload")
 
-  workerCount := flag.Int("c", 3, "Count of workers (default: 3)")
+  workerCount := flag.Int("c", 3, "Count of workers")
   contentType := flag.String("H", "text/plain", "Content-Type")
   // mode        := flag.String("fmode", "and", "Filter set operator. Either of: and, or (default: and)")
   regexp      := flag.String("fr", "", "Filter regexp")
   // time        := flag.String("ft", "", "Filter by number of milliseconds to the first response byte, either greater or less than. EG: >100 or <100")
-  lines       := flag.String("fl", "", "Filter HTTP status codes from response. Comma separated list of codes and ranges")
+  lines       := flag.String("fl", "", "Filter by amount of lines in response. Comma separated list of line counts and ranges")
   size        := flag.String("fs", "", "Filter HTTP response size. Comma separated list of sizes and ranges")
   status      := flag.String("fc", "", "Filter HTTP status codes from response. Comma separated list of codes and ranges")
   words       := flag.String("fw", "", "Filter by amount of words in response. Comma separated list of word counts and ranges")
@@ -73,6 +71,7 @@ func GetConfig() (result *Config, err error) {
     return
   }
 
+  result = &Config{}
   result.Target        = *target
   result.WorlistPath   = *worlistPath
   result.Payload       = *payload
